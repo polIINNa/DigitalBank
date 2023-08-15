369e3c83e2b0141b45d3a2718b25e522308a6ed6
Реброва Полина Андеевна <PAnRebrova@sberbank.ru>
2023-08-15 22:50:19 +0300

    some refactor

--- digital_bank.py
+++ digital_bank.py
@@ -2,13 +2,36 @@ from abc import ABC, abstractmethod
 
 
 class SystemUser(ABC):
+    #использование слотов для экономии памяти (и + в задании сказано использовать слоты, так что имеет место быть
+    __slots__ = ('f_name', 'l_name', 'age', 'login', 'password')
+
     @abstractmethod
     def __init__(self, f_name: str, l_name: str, age: int, login: str, password: str):
-        self.f_name = f_name
-        self.l_name = l_name
-        self.age = age
-        self.login = login
-        self.password = password
+        self._f_name = f_name
+        self._l_name = l_name
+        self._age = age
+        self._login = login
+        self._password = password
+
+    @property
+    def login(self):
+        return self._login
+
+    @login.setter
+    def login(self, new_login):
+        self._login = new_login
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, new_password):
+        self._password = new_password
+
+    def change_password(self, old_password, new_password):
+        if self.password == old_password:
+            self.password = new_password
 
 
 class DigitalWallet:
@@ -19,40 +42,55 @@ class DigitalWallet:
         return self.balance
 
     def deposit_money(self, val):
+        """
+        фунция для внесения суммы на электронный кошелек
+        :param val: сумма пополнения
+        :return: None
+        """
         self.balance += val
 
     def withdraw_money(self, val):
+        """
+        функция для списания суммы с электронного кошелька
+        :param val: сумма списания
+        :return: None
+        """
         if val <= self.balance:
             self.balance -= val
 
 
 class User(SystemUser, DigitalWallet):
-    def __init__(self, f_name, l_name, age, login, password):
+    def __init__(self, f_name: str, l_name: str, age: int, login: str, password: str):
         SystemUser.__init__(self, f_name=f_name, l_name=l_name, age=age, login=login, password=password)
-        self.wallet = DigitalWallet()
+        self._wallet = DigitalWallet()
 
     def __str__(self):
-        return self.f_name + ' ' + self.l_name
+        return self._f_name + ' ' + self._l_name + ' ' + str(self._age)
+
+    @property
+    def wallet(self):
+        return self._wallet
 
 
-user = User('Polina', 'Rebrova', 21, 'qwerty', '123')
-print(str(user))
-user.wallet.deposit_money(1000)
-user.wallet.withdraw_money(600)
-print(user.wallet.get_balance())
-
-
-class Admin(SystemUser):
-    def __init__(self, f_name, l_name, age, login, password):
+class Admin(SystemUser, DigitalWallet):
+    def __init__(self, f_name: str, l_name: str, age: int, login: str, password: str):
         SystemUser.__init__(self, f_name=f_name, l_name=l_name, age=age, login=login, password=password)
 
     def __str__(self):
-        return self.f_name + ' ' + self.l_name
+        return self._f_name + ' ' + self._l_name
 
     @staticmethod
     def get_user_info(usr: User):
-       return str(usr), usr.login, usr.password
+       return str(usr), usr._login, usr._password
 
     @staticmethod
     def get_user_wallet(usr: User):
-        return usr.wallet.get_balance()
+        """
+        метод позволяет проводить все возможные операции с электронным кошелком пользователя
+        :param usr: экземпляр класса User (пользователь)
+        :return: электронный колшелек пользователя
+        """
+        return usr.wallet
+
+
+        
